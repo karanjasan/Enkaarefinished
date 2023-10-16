b4ea47d854c6a4c6381ded8ea9bc53a57490a8b1
antomunene <antomunene23@gmail.com>
2023-10-16 06:41:47 -0700

    repush

---
+++ signup-q.js
@@ -0,0 +1,14 @@
+let form=document.getElementById("form");
+let  freelance=document.getElementById("freelancer");
+let hire=document.getElementById("hire");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if(freelance.checked===true){
+        window.location.href="/signupwork";
+        form.reset()
+    }else if(hire.checked===true){
+        window.location.href="/sighnuphire";
+        form.reset()
+    }
+})
